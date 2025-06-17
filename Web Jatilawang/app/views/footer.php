d2cab906b4e9259222c353f78265e59ab2e85288
Brifly Anthon Wattimury <156288536+Bilabong29@users.noreply.github.com>
2025-06-18 00:37:15 +0700

    Delete Web Jatilawang directory

--- Web Jatilawang/app/views/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="main-footer">
-    <p>&copy; <?php echo date("Y"); ?> Jatilawang Adventure. Hak Cipta Dilindungi.</p>
-  </footer>
-</body>
-</html>
--- Web Jatilawang/logout.php
+++
@@ -1,8 +0,0 @@
-<!-- logout.php -->
-<?php
-// Logout Logic
-session_start();
-session_destroy();
-header('Location: index.php');
-exit;
-?>
